ec6ad7b1d513787d44cc06bf23359820c72152d5
kemandel <mandel01@allegheny.edu>
2024-08-16 17:14:22 -0400

    Made player decelerate when no movement is input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,13 +9,17 @@ public class PlayerController : MonoBehaviour
     private float basePlayerSpeed;
     [SerializeField]
     private float basePlayerAcceleration;
+    [SerializeField]
+    private float decelerationScalar = 1;
 
     private float currentVelocity;
+    private Vector2 currentDirection;
 
     // Start is called before the first frame update
     void Start()
     {
         currentVelocity = 0;
+        currentDirection = Vector2.zero;
     }
 
     // Update is called once per frame
@@ -25,16 +29,20 @@ public class PlayerController : MonoBehaviour
         Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
         float playerSpeed = GetCurrentPlayerSpeed();
 
-        if (direction != Vector2.zero) currentVelocity += basePlayerAcceleration * Time.deltaTime;
-        else currentVelocity -= basePlayerAcceleration * Time.deltaTime;
+        if (direction != Vector2.zero)
+        {
+            currentDirection = direction;
+            currentVelocity += basePlayerAcceleration * Time.deltaTime;
+        }
+        else currentVelocity -= basePlayerAcceleration * decelerationScalar * Time.deltaTime;
 
         if (currentVelocity > playerSpeed) currentVelocity = playerSpeed;
         if (currentVelocity < 0) currentVelocity = 0;
 
-        if (direction != Vector2.zero) Debug.Log("Current Speed: " + currentVelocity); 
-        transform.Translate(currentVelocity * Time.deltaTime * direction);
+        if (currentVelocity != 0) Debug.Log("Current Speed: " + currentVelocity); 
+        transform.Translate(currentVelocity * Time.deltaTime * currentDirection);
 
-        
+
     }
 
     private float GetCurrentPlayerSpeed()
