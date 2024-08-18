f3245949a77c51ede87168c1f99f1977905fa81e
kemandel <mandel01@allegheny.edu>
2024-08-17 23:32:37 -0400

    Fixed camera not resetting with scene load

--- Assets/Scripts/Gameplay/CameraManager.cs
+++ Assets/Scripts/Gameplay/CameraManager.cs
@@ -8,11 +8,14 @@ public class CameraManager : MonoBehaviour
     public float endingScale = 15;
     public float zoomTime = 60;
 
+    private float currentTime;
+
     public static Vector2 EdgeExtents { get; private set; }
 
     // Start is called before the first frame update
     void Awake()
     {
+        currentTime = 0;
         transform.localScale = new Vector3(startingScale, startingScale, 1);
         UpdateEdgeExtents();
     }
@@ -20,11 +23,13 @@ public class CameraManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float ratio = Mathf.Pow(Mathf.Clamp01(Time.time / zoomTime), 1.5f);
+        float ratio = Mathf.Pow(Mathf.Clamp01(currentTime / zoomTime), 1.5f);
         float newScale = Mathf.Lerp(startingScale, endingScale, ratio);
 
         transform.localScale = new Vector3(newScale, newScale, 1);
         UpdateEdgeExtents();
+
+        currentTime += Time.deltaTime;
     }
 
     private void UpdateEdgeExtents()
