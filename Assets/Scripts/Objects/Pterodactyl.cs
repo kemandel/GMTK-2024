64c3efce2f6e602b41c5267447d37ef966dcc32a
kemandel <mandel01@allegheny.edu>
2024-08-20 03:50:49 -0400

    Added flyer

---
+++ Assets/Scripts/Objects/Pterodactyl.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pterodactyl : Enemy
+{
+    public float verticalOffset = 4;
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        // Skip movement during anims
+        if (spawning || player == null) return;
+
+        if (!attacking)
+        {
+            float distanceToGoal = Vector3.Distance(transform.position + new Vector3(0, verticalOffset, 0), player.transform.position);
+            if (distanceToGoal < attackRange)
+            {
+                StartCoroutine(AttackCoroutine());
+            }
+            else
+            {
+                Move();
+            }
+        }
+        else
+        {
+            Move();
+        }
+    }
+
+    public override void Move()
+    {
+        Vector2 direction = (player.transform.position + new Vector3(0, verticalOffset * (attacking ? -.5f : 1), 0) - transform.position).normalized;
+        if (!attacking) mySpriteRenderer.flipX = direction.x > 0;
+        transform.Translate((attacking ? 2 : 1) * moveSpeed * Time.deltaTime * direction);
+    }
+}
